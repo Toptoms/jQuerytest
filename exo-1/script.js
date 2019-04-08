2c9a0e4f034adce2aeaf7216955ac74d52bb968e
Tom <thomas.bozon@campu_numerique.fr>
2019-04-08 11:35:30 +0200

    Exercice 1 : Au commencement ok

---
+++ exo-1/script.js
@@ -0,0 +1,9 @@
+console.log('exercice 1');
+
+//JQuery est une librairie de code Javascript simplifié. Il permet de faire les mêmes choses que JavaScript (API)
+// en écrivant beaucoup moins de code.
+
+$(document).ready(function(){
+    
+	console.log('Je peux maintenant écrire du code JQuery');
+});
