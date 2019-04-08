2e558aba377fdad45e9ff90a3df85ed1165c79dd
Tom <thomas.bozon@campu_numerique.fr>
2019-04-08 14:36:23 +0200

    Exercice 2 : Je sélectionne, tu sélectionnes, il sélectionne, nous sélectionnons ok

--- exo-2/script.js
+++ exo-2/script.js
@@ -1,2 +1,14 @@
 console.log('exercice 2');
 
+$('#title'),
+$('.sidenav').hide(),
+$('footer').show(),
+$('.input-group'),
+$('.glyphicon'),
+$('.label-success'),
+$('.glyphicon-time'),
+$('a[href="#section1"]'),
+$('p:even')
+
+
+
