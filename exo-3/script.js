f67e2daf6870d01c4b1972f19559aa39d847ca6e
Tom <thomas.bozon@campu_numerique.fr>
2019-04-08 17:04:00 +0200

    Exercice 3 : Manipulation 3.4  en cours

--- exo-3/script.js
+++ exo-3/script.js
@@ -1,1 +1,15 @@
 console.log("exercice 3");
+
+
+let $jQuerytitre = $('h1');
+let $jQuerytagline = $('#tagline');
+let $jQuerygly = $('glyphicon-log-in');
+glyphicon glyphicon-glass
+
+$(document).ready(function () {
+    console.log('Je peux maintenant écrire du code JQuery')
+    $jQuerytitre.text('tomtom')
+    $jQuerytagline.text('jQuery it is the life')
+    $('#myNavbar > ul:first').remove()
+});
+
