165c5cd483fb13d3ce79b3ea50539502b7917900
Tom <thomas.bozon@campu_numerique.fr>
2019-04-12 17:19:54 +0200

    Exercice 6 : Un cas très classique en web : contrôler les données d'un formulaire ok

--- exo-6/script.js
+++ exo-6/script.js
@@ -1,1 +1,31 @@
 console.log("exercice 6");
+function checkLogin(mail) {
+
+
+};
+
+$(document).ready(() => {
+
+    $("#mail").hide()
+    $("#pass").hide()
+
+    $('#send').on('click', () => {
+        alert('youpi');
+        let mail = $('#InputEmail').val();
+        let pass = $('#InputPassword').val();
+        checkmail (mail, pass)
+    })
+
+})
+
+checkmail = ((mail, pass) => {
+    if (mail === '') {
+        console.log(mail)
+        $("#mail").show()
+    }
+
+    if (pass === '') {
+        console.log(pass)
+        $("#pass").show()
+    }
+})
