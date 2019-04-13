c5278add031c2f9a37f60156ee022f85a32a74b0
toto <thomas.bozon@le-campus-numerique.fr>
2019-04-13 15:48:41 +0200

    Exercice 6 : Un cas très classique en web : contrôler les données d'un formulaire ok

--- exo-6/script.js
+++ exo-6/script.js
@@ -1,31 +1,41 @@
 console.log("exercice 6");
-function checkLogin(mail) {
-
-
-};
 
 $(document).ready(() => {
+    click()
+})
 
-    $("#mail").hide()
-    $("#pass").hide()
-
+click = () => {
     $('#send').on('click', () => {
-        alert('youpi');
         let mail = $('#InputEmail').val();
         let pass = $('#InputPassword').val();
-        checkmail (mail, pass)
+        isCheck(mail, pass)
+
     })
+}
 
-})
-
-checkmail = ((mail, pass) => {
-    if (mail === '') {
-        console.log(mail)
-        $("#mail").show()
-    }
+isCheck = ((mail, pass) => {
+    let passok = 0
+    let mailok = 0
+    var regex = /^([a-zA-Z0-9_\.\-\+])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+    if (mail === '' || !regex.test(mail)) {
+        $("#mailvalid").removeClass('d-none')
+    } else $("#mailvalid").addClass('d-none'), mailok = 1;
 
     if (pass === '') {
-        console.log(pass)
-        $("#pass").show()
+        $("#pass.d-none").removeClass('d-none')
+    } else $("#pass").addClass('d-none')
+
+    if (pass.length < 6) {
+        $("#passsixt").removeClass('d-none')
+    } else $("#passsixt").addClass('d-none'), passok = 1;
+
+    if (mail === 'hello@me.com' && pass === 'secret8') {
+        alert('**Vous êtes connecté**')
+    } else if (mailok===1 && passok===1){
+        alert('ton compte est accepté ')
     }
+
 })
+
+
+
