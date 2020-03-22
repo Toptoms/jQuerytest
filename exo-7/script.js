6c670b443c726079051a3f9d03eff7d3ca3e093e
tbozon <tbozon@editions-tissot.fr>
2020-03-22 12:27:20 +0100

    Données sous format JSON bingo

--- exo-7/script.js
+++ exo-7/script.js
@@ -1,124 +1,309 @@
-console.log("exercice 7");
-var nbrline = 50;
-var startline = 1;
-var useData = data;
-var nbrpages = useData.length / nbrline;
-var dataSearch;
+console.log('exercice 7');
 
-function getData() {
-  return data; // data is defined in DATA.js file
+let allDatas = [];
+let currentDatas = [];
+let currentPage = 1;
+let currentRadio = -1;
+let nextIndex = -1;
+let valueBeforeEditing;
+let idDataEditing;
+
+function displayDatas() {
+    let $newTbody = $('<tbody>');
+    let firstData = (currentPage - 1) * 50;
+    let latestData = (currentPage * 50 <= currentDatas.length) ? (currentPage * 50) : currentDatas.length;
+    for (let iterate = firstData; iterate < latestData; iterate++) {
+        $newTbody.append(addLineToTable(currentDatas[iterate]));
+    }
+    $('tbody').replaceWith($newTbody);
+    displayPagination();
+}
+
+function addLineToTable(data) {
+    let newLine = $('<tr>');
+    newLine.append(createCell(data.id, null, 'radio'));
+    newLine.append(createCell(data.id));
+    newLine.append(createCell(data.name.last, data.id));
+    newLine.append(createCell(data.name.first, data.id));
+    newLine.append(createCell(data.email, data.id));
+    newLine.append(createCell(data.phone, data.id));
+    return newLine;
+}
+
+function createCell(data, id = null, type = null) {
+    let newCell = $('<td>');
+    if (!type) {
+        newCell.text(data);
+        if (id) {
+            newCell.attr('id', id);
+            newCell.on('click', function () {
+                startEditObject($(this));
+            });
+        }
+        return newCell;
+    }
+    let newRadio = $('<input>');
+    newRadio.attr('type', type);
+    newRadio.val(data);
+    newRadio.attr('name', 'radio');
+    if (data === currentRadio) {
+        newRadio.attr('checked', true);
+    }
+    newRadio.on('click', function () {
+        currentRadio = data;
+        $('#deleteButton').removeAttr('disabled');
+    });
+    newCell.append(newRadio);
+    return newCell;
+}
+
+function displayPagination() {
+    $('nav ul').remove();
+    if (countPage(currentDatas) !== 1) {
+        let pagination = $('<ul>');
+        pagination.addClass('pagination pagination-sm justify-content-center');
+        for (let iterate = 1; iterate <= countPage(currentDatas); iterate++) {
+            pagination.append(createLi(iterate));
+        }
+        $('nav').append(pagination);
+    }
+}
+
+function createLi(page) {
+    let li = $('<li>');
+    (page === currentPage) ? li.addClass('page-item disabled') : li.addClass('page-item');
+    let a = $('<a>');
+    (page === currentPage) ? a.addClass('page-link bg-primary text-white') : a.addClass('page-link');
+    a.attr('href', '#');
+    a.text(page);
+    a.on('click', function () {
+        currentPage = page;
+        displayDatas();
+    });
+    li.append(a);
+    return li;
+}
+
+function countPage(datas) {
+    return (Math.ceil(datas.length / 50));
+}
+
+function getFilterDatas() {
+    let filtredDatas = [];
+    let filter = $('#searchInput').val();
+    let regExp = new RegExp(filter.toLowerCase());
+    allDatas.forEach(function (data) {
+        if (regExp.test(data.name.first.toLowerCase()) ||
+            regExp.test(data.name.last.toLowerCase()) ||
+            regExp.test(data.email.toLowerCase()) ||
+            regExp.test(data.phone.toLowerCase())) {
+            filtredDatas.push(data);
+        }
+    });
+    return filtredDatas;
+}
+
+function updateHeadTable(element) {
+    let $element = $('#' + element + ' i');
+    let memoryState = $element.hasClass('fa-sort-asc');
+    let $elements = $('th i');
+    $elements.removeClass();
+    $elements.addClass('fa fa-sort');
+    memoryState ? $element.addClass('fa fa-sort-desc') : $element.addClass('fa fa-sort-asc');
+    sortDatas(element, memoryState ? 'desc' : 'asc');
+    displayDatas();
+}
+
+function sortDatas(column, order) {
+    currentDatas.sort(compareValues(column, order));
+}
+
+function compareValues(key, order = 'asc') {
+    return function (a, b) {
+        let varA;
+        let varB;
+        let comparison = 0;
+        let underKey;
+        switch (key) {
+            case 'firstname':
+                underKey = 'first';
+                varA = a['name'][underKey].toUpperCase();
+                varB = b['name'][underKey].toUpperCase();
+                break;
+            case 'lastname':
+                underKey = 'last';
+                varA = a['name'][underKey].toUpperCase();
+                varB = b['name'][underKey].toUpperCase();
+                break;
+            default:
+                if (!a.hasOwnProperty(key) || !b.hasOwnProperty(key)) {
+                    return 0;
+                }
+                varA = (typeof a[key] === 'string') ? a[key].toUpperCase() : a[key];
+                varB = (typeof b[key] === 'string') ? b[key].toUpperCase() : b[key];
+        }
+        if (varA > varB) {
+            comparison = 1;
+        } else if (varA < varB) {
+            comparison = -1;
+        }
+        return ((order === 'desc') ? (comparison * -1) : comparison);
+    };
+}
+
+function addData() {
+    let data =
+        {
+            "id": nextIndex,
+            "name": {
+                "first": $('#firstnameInput').val(),
+                "last": $('#lastnameInput').val()
+            },
+            "email": $('#emailInput').val(),
+            "gender": $('#genderInput').val(),
+            "phone": $('#phoneInput').val(),
+            "country": $('#countryInput').val()
+        };
+    allDatas.push(data);
+    nextIndex++;
+}
+
+function hideAddForm() {
+    $('.mask').css('display', 'none');
 }
 
 $(document).ready(function () {
-  displayDatas(data)
-})
+    allDatas = getData();
+    nextIndex = allDatas.length + 1;
 
-function displayDatas(data) {
-  addLineTotable(data);
-  addPagination();
-}
-$('i').on('click', (event) => {
-  let $target = $(event.currentTarget);
-  let $sortclass=$target.attr('id');
-  console.log($sortclass)
-
-  if ($target.hasClass('fa-sort-up')) {
-    
-    $('i').removeClass().toggleClass('fas fa-sort')
-     $target.toggleClass('fa-sort-down')
-    useData.reverse(function (a, b) {
-      return a[$sortclass].localeCompare(b[$sortclass]) 
-    })
-    displayDatas(useData)
-  }
-
-  else if ($target.hasClass('fa-sort-down')) {
-    $('i').removeClass().toggleClass('fas fa-sort')
-    useData.sort(function (a, b) {
-      return a['id']-(b['id'])
-    })
-    displayDatas(useData)
-  }
-
-  else {
-    $('i').removeClass().toggleClass('fas fa-sort')
-     $target.toggleClass('fa-sort-up')
-    useData.sort(function (a, b) {
-      return a[$sortclass].localeCompare(b[$sortclass])
-    })
-    displayDatas(useData)
-  };
-  console.log($target.attr('id'))
-});
-
-$('#search').on('keyup', function () {
-  searchData()
-  useData = dataSearch
-  nbrpages = dataSearch.length / nbrline
-  displayDatas(useData)
-});
-
-function addLineTotable(d) {
-  let ip = (startline - 1) * nbrline;
-  let l = nbrline * startline - 1;
-  if (l > d.length) {
-    l = d.length - 1
-  }
-  $("tbody").empty()
-  for (i = ip; i <= l; i++) {
-    $("tbody").append(
-      "<tr>\
-    <td>"+ [i + 1] + "</td>\
-    <td>"+ d[i].id + "</td>\
-    <td>"+ d[i].email + "</td>\
-    <td>"+ d[i].name.first + "</td>\
-    <td>"+ d[i].name.last + "</td>\
-    <td>"+ d[i].phone + "</td>\
-    </tr>"
-    );
-  };
-}
-
-addPagination = () => {
-  $nbrp = $('.pagination');
-  $nbrp.empty();
-  let nrb = Math.ceil(nbrpages)
-  for (let i = 1; i <= nrb; i++) {
-    $($nbrp).append(
-      "<li class='page-item'><a class='page-link' href='#' id=" + [i] + ">" + [i] + "</a></li>");
-    $('a').on('click', function () {
-      startline = this.id;
-      displayDatas(useData);
+    $('#refreshButton').on('click', function () {
+        $('#searchInput').val('');
+        currentPage = 1;
+        currentDatas = allDatas;
+        displayDatas();
     });
-  }
-};
+    $('#searchButton').on('click', function () {
+        currentPage = 1;
+        currentDatas = getFilterDatas();
+        displayDatas();
+    });
+    $('th').on('click', function () {
+        if ($(this).find('i').hasClass('fa')) {
+            updateHeadTable($(this).attr('id'));
+        }
+    });
+    $('#newButton').on('click', function () {
+        $('.mask input').val("");
+        $('.mask').css('display', 'flex');
+    });
+    $('#cancelButton').on('click', function () {
+        hideAddForm();
+    });
+    $('#deleteButton').on('click', function () {
+        for (let index = 0; index < allDatas.length; index++) {
+            if (allDatas[index].id === currentRadio) {
+                allDatas.splice(index, 1);
+                currentRadio = -1;
+                $('#deleteButton').attr('disabled', 'disabled');
+                displayDatas();
+                return true;
+            }
+        }
+    });
+    $('#addForm').on("submit", function (event) {
+        event.preventDefault();
+        addData();
+        displayDatas();
+        hideAddForm();
+    });
+    $('#downloadButton').on('click', function () {
+        exportToCsv();
+    });
+    currentDatas = allDatas;
+    displayDatas();
+});
 
-searchData = () => {
-  dataSearch = []
-  var compare = $('#search').val()
-  data.forEach(function (element) {
-    if (element.name.first.toLowerCase().search(compare.toLowerCase()) === 0)/*||
-      element.name.last.toLowerCase().search(compare.toLowerCase()) === 0)*/ {
-      dataSearch.push(element);
-    }
-  })
-};
+function getData() {
+    return data; // data is defined in DATA.js file
+}
 
+function startEditObject($object) {
+    // Save value before editing
+    valueBeforeEditing = $object.text();
+    idDataEditing = $object.attr('id');
 
-loopData = () => {
-  /*for (let i = 0; i < useData.length; i++) {
-    console.log(useData[i].name.first)
-  };*/
+    // Create input to edit object
+    let $inputText = $('<input>');
+    $inputText.attr('type', 'text');
+    $inputText.attr('id', 'editObject');
+    $inputText.addClass('form-control');
+    $inputText.val(valueBeforeEditing);
+    let $editingObject = $('<td>');
+    $editingObject.append($inputText);
 
-  useData.forEach(function (element) {
-    console.log(element.name.first)
-  });
+    // Replace current object by input
+    $object.replaceWith($editingObject);
 
-  /*for (let element in useData) {
-     console.log(useData[element].name.first)
-   };*/
+    // Add events
+    let $editObject = $('#editObject');
+    $editObject.on('focusout', function (event) {
+        updateData($(event.currentTarget).val());
+        displayDatas();
+    });
+    $editObject.on('keydown', function (event) {
+        if (event.keyCode === 13) {
+            updateData($(event.currentTarget).val());
+            displayDatas();
+        }
+    });
+    // Set focus into input
+    $editObject.focus();
+}
 
-  /*$.each(useData, function (index,element){
-    console.log(element.name.first)
-   });*/
+function updateData(value) {
+    currentDatas.forEach(function (data) {
+        if (data.id === parseInt(idDataEditing)) {
+            if (data.name.first === valueBeforeEditing) {
+                data.name.first = value;
+                return true;
+            } else if (data.name.last === valueBeforeEditing) {
+                data.name.last = value;
+                return true;
+
+            } else if (data.email === valueBeforeEditing) {
+                data.email = value;
+                return true;
+            } else if (data.phone === valueBeforeEditing) {
+                data.phone = value;
+                return true;
+            } else {
+                return false;
+            }
+        }
+    });
+}
+
+function exportToCsv() {
+    let csv = createArray().map(function (row) {
+        return row.join(',');
+    }).join('\n');
+
+    let hiddenElement = document.createElement('a');
+    hiddenElement.href = 'data:text/csv;charset=utf-8,' + encodeURI(csv);
+    hiddenElement.target = '_blank';
+    hiddenElement.download = 'download.csv';
+    hiddenElement.click();
+}
+
+function createArray() {
+    let array = [];
+    currentDatas.forEach(function (data) {
+        array.push([
+            String(data.name.last),
+            String(data.name.first),
+            String(data.email),
+            String(data.phone)
+        ]);
+    });
+    return array;
 }
--- exo-8/script.js
+++ exo-8/script.js
@@ -1,1 +1,9 @@
 console.log("exercice 8");
+
+$(document).ready(function () {
+    $('.group1').colorbox({rel: 'group1', transition: "fade", width: "75%", height: "75%"});
+
+    $('#datepicker').datepicker({
+        inline: true
+    });
+});
