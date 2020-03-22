27aa5cf0ba2e5d383af9c6ccef5b895616970e6d
tbozon <tbozon@editions-tissot.fr>
2020-03-22 13:31:52 +0100

    plugin jQuery

--- exo-8/script.js
+++ exo-8/script.js
@@ -1,9 +1,7 @@
 console.log("exercice 8");
 
 $(document).ready(function () {
-    $('.group1').colorbox({rel: 'group1', transition: "fade", width: "75%", height: "75%"});
+    $('.group').colorbox({rel: 'group', transition: "fade", width: "75%", height: "75%"});
 
-    $('#datepicker').datepicker({
-        inline: true
-    });
+
 });
